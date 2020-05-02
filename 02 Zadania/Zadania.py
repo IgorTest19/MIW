284eaaeff5d29c53ef06e9ee2bb1f6eaa1f5a546
IgorTest19 <58894110+IgorTest19@users.noreply.github.com>
2020-05-02 23:52:38 +0200

    Update Zadania.py

--- 02 Zadania/Zadania.py
+++ 02 Zadania/Zadania.py
@@ -1,9 +1,11 @@
 from file_manager import FileManager
 
-#Zad1
+# Zad1
 
-a_list = [0,1,2,3,4]
+a_list = [0, 1, 2, 3, 4]
 b_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+
 def Zad1(a_list, b_list):
     c_list = []
     i = 0
@@ -18,11 +20,12 @@ def Zad1(a_list, b_list):
             c_list.append(i)
         i += 1
     return c_list
-print(Zad1(a_list,b_list))
 
 
+# print(Zad1(a_list,b_list))
 
-#Zad2
+
+# Zad2
 
 def zad2(data_text):
     length = len(data_text)
@@ -34,26 +37,29 @@ def zad2(data_text):
         "letters: ": letters,
         "big_letters: ": big_letters,
         "small_letters: ": small_letters
-            }
-
-print(Zad2("Metody Inżynierii Wiedzy"))
+    }
 
 
-#Zad3
+# print(zad2("Metody Inżynierii Wiedzy"))
+
+
+# Zad3
 def zad3(text, letter):
     return text.replace(letter, '')
 
-print(Zad3("Metody Inżynierii Wiedzy", 'i'))
+
+# print(zad3("Metody Inżynierii Wiedzy", 'i'))
 
 
 def zad3(text, letter):
     slowo = [i for i in text if i != letter.upper() and i != letter.lower()]
     return ''.join(slowo)
 
-print(Zad3("Metody Inżynierii Wiedzy", 'i'))
+
+# print(zad3("Metody Inżynierii Wiedzy", 'i'))
 
 
-#Zad4
+# Zad4
 
 def zamiana_stopni(temperature_type):
     if temperature_type < -273.15:
@@ -62,13 +68,15 @@ def zamiana_stopni(temperature_type):
         fahrenheit = temperature_type * 1.8 + 32
         rankine = (temperature_type + 273.15) * 1.8
         kelvin = temperature_type + 273.15
-        zamiana = "{0:.2f} stopni Celsjusza to {1:.2f} Fahreheita, {2:.2f} Rankine i {3:.2f} Kelvina".format(temperature_type, fahrenheit, rankine, kelvin)
+        zamiana = "{0:.2f} stopni Celsjusza to {1:.2f} Fahreheita, {2:.2f} Rankine i {3:.2f} Kelvina".format(
+            temperature_type, fahrenheit, rankine, kelvin)
     return zamiana
 
-print(zamiana_stopni(-273.15))
+
+# print(zamiana_stopni(-273.15))
 
 
-#Zad5
+# Zad5
 
 class Calculator:
     """
@@ -85,7 +93,7 @@ class Calculator:
 
     def __init__(self, liczba_1, liczba_2):
         self.liczba_1 = liczba_1
-        self.liczba_2  = liczba_2
+        self.liczba_2 = liczba_2
 
     def add(self):
         return self.liczba_1 + self.liczba_2
@@ -103,6 +111,7 @@ class Calculator:
             wynik = "Podzielono przez zero... (╯ ͠° ͟ʖ ͡°)╯┻━┻"
         return wynik
 
+
 # kalkulator = Calculator(1,0)
 # print(kalkulator.add())
 # print(kalkulator.difference())
@@ -110,9 +119,7 @@ class Calculator:
 # print(kalkulator.divide())
 
 
-
-
-#Zad6
+# Zad6
 
 class ScienceCalculator(Calculator):
     """
@@ -131,10 +138,10 @@ class ScienceCalculator(Calculator):
     #     return self.liczba_1
 
     def powers(self):
-        return self.liczba_1**self.liczba_2
+        return self.liczba_1 ** self.liczba_2
 
     def root(self):
-        return self.liczba_1**(1/self.liczba_2)
+        return self.liczba_1 ** (1 / self.liczba_2)
 
 
 # sc = ScienceCalculator(2,3)
@@ -145,7 +152,7 @@ class ScienceCalculator(Calculator):
 # print(sc2.root())
 
 
-#Zad7
+# Zad7
 
 def reverse(tekst):
     """
@@ -155,16 +162,13 @@ def reverse(tekst):
     """
     return tekst[::-1]
 
+
 print(reverse("koteł"))
 
-
-#Zad8/9
+# Zad8/9
 
 fm = FileManager("test.txt")
 fm.read_file()
 fm.update_file("Nowy tekst")
 
-
-#Zad10
-
-
+# Zad10
